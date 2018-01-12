7570b63265443039564c89940902a29ff68c4a0c
Erik Ripley <erikripley@gmail.com>
2018-01-11 20:32:23 -0600

    Don't use auth if credentials not specified

--- play_stream.sh
+++ play_stream.sh
@@ -4,13 +4,20 @@ echo "You can run this in the background with:"
 echo "tmux new-session -d `basename ${0}`"
 echo
 
-if [ -z ${PLAY_STREAM_URL} ] || [ -z ${PLAY_STREAM_AUTH} ]; then
-  echo "Variables not set. Use 'echo -n myusername:mypassword | base64 -e' to encode basic auth. Exiting..."
+#Use 'echo -n myusername:mypassword | base64' to encode basic auth.
+
+if [ -z ${PLAY_STREAM_URL} ]; then
+  echo "PLAY_STREAM_URL not set. Exiting..."
   exit 1
 fi
 
 while true; do
-  mpv --http-header-fields="Authorization:Basic ${PLAY_STREAM_AUTH}" --no-video --no-ytdl ${PLAY_STREAM_URL}
+  if [ ! -z ${PLAY_STREAM_AUTH} ]; then 
+    mpv --http-header-fields="Authorization:Basic ${PLAY_STREAM_AUTH}" --no-video --no-ytdl ${PLAY_STREAM_URL}
+  else
+    echo "PLAY_STREAM_AUTH not set." 
+    mpv --no-video --no-ytdl ${PLAY_STREAM_URL}
+  fi
   echo "sleeping for 10 seconds..."
   sleep 10
 done
