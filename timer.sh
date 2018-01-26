b766e3d0bfade22811b57eae2b628a1a84c9b504
ripperbone <erikripley@gmail.com>
2018-01-25 19:12:52 -0600

    Colorize timer output, fix notify when done on mac

--- timer.sh
+++ timer.sh
@@ -4,6 +4,19 @@ format-time() {
    printf "%02d:%02d:%02d\n" $((${1} / 3600)) $((${1} % 3600 / 60))  $((${1} % 60))
 }
 
+
+notify-finished() {
+  which notify-send >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    notify-send "Done." "The timer has finished."
+  fi
+
+  which terminal-notifier >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    terminal-notifier -title "Timer" -message "The timer has finished." 
+  fi
+}
+
 usage() {
 cat <<-eos 
    Usage: timer.sh [number of minutes]
@@ -24,16 +37,22 @@ if [ $? -ne 0 ]; then
    exit 1
 fi
 
+which lolcat >/dev/null 2>&1
+USE_COLORS=$?
 
 while [ ${TIME_LEFT} -gt 0 ]; do
 
-   format-time ${TIME_LEFT}
+   if [ ${USE_COLORS} -eq 0 ]; then
+      format-time ${TIME_LEFT} | lolcat
+   else
+      format-time ${TIME_LEFT}
+   fi
 
    # wait 1 second and decrement counter
    sleep 1
    TIME_LEFT=`expr ${TIME_LEFT} - 1`
 done
 
-notify-send "Done." "The timer has finished."
+notify-finished
 echo "Done."
 exit 0
