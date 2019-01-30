e5c046503f96fc7594b661cc1f204cbdaf73259b
Erik Ripley <erikripley@gmail.com>
2019-01-29 20:59:57 -0600

    allow force deleting branches

--- git-dump-branches.rb
+++ git-dump-branches.rb
@@ -2,6 +2,6 @@
 
 `git branch`.split("\n").each do |branch|
    next if branch.strip.include? '*' or branch.strip.include? 'master'
-   puts "git branch -d #{branch.strip}"
-   system("git branch -d #{branch.strip}")
+   puts "git branch #{ARGV.include?('-D') ? '-D' : '-d'} #{branch.strip}"
+   system("git branch #{ARGV.include?('-D') ? '-D' : '-d'} #{branch.strip}")
 end
