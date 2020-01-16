ee8dfbc0e07ee1ed92275c897d30aba5e8477d1c
Erik Ripley <erikripley@gmail.com>
2020-01-16 01:15:26 -0600

    set desktop wallpaper for gnome

--- set_desktop_wallpaper.sh
+++ set_desktop_wallpaper.sh
@@ -2,17 +2,36 @@
 
 # The desktop images I choose are not always preserved. Specify path to desired image and update on all monitors.
 
-DESKTOP_PICTURE="/Library/Desktop Pictures/Abstract Shapes.jpg"
 
-if ! which osascript >/dev/null 2>&1; then
-  echo "Can't find Applescript interpreter. Are we on a mac?"
-  exit 1
-fi
+set_wallpaper_gnome() {
+   DESKTOP_PICTURE="$1"
 
-osascript <<EOF
+   if ! which gsettings >/dev/null 2>&1; then
+      echo "Can't find gsettings. Is this GNOME?"
+      exit 1
+   fi
+
+   gsettings set org.gnome.desktop.background picture-uri "file://${DESKTOP_PICTURE// /%20}"
+   gsettings set org.gnome.desktop.screensaver picture-uri "file://${DESKTOP_PICTURE// /%20}"
+}
+
+
+set_wallpaper_mac() {
+   DESKTOP_PICTURE="$1"
+
+   if ! which osascript >/dev/null 2>&1; then
+     echo "Can't find Applescript interpreter. Are we on a mac?"
+     exit 1
+   fi
+
+   osascript <<EOF
 tell application "System Events"
   tell every desktop
     set picture to "$DESKTOP_PICTURE"
   end tell
 end tell
 EOF
+}
+
+
+set_wallpaper_gnome "/usr/share/backgrounds/gnome/Icescape.jpg"
