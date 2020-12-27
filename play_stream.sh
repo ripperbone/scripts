b2325349581761880afb95a72841e55482f77a42
Erik Ripley <erikripley@gmail.com>
2020-12-27 13:50:35 -0600

    don't try to reconnect when --no-loop is specified

--- play_stream.sh
+++ play_stream.sh
@@ -4,6 +4,10 @@ echo "You can run this in the background with:"
 echo "tmux new-session -d $(basename "${0}")"
 echo
 
+if [ "$1" = "--no-loop" ]; then
+   LOOP=no
+fi
+
 #Use 'echo -n myusername:mypassword | base64' to encode basic auth.
 
 if [ -z "${PLAY_STREAM_URL}" ]; then
@@ -16,12 +20,18 @@ if ! mpv --version; then
 fi
 
 while true; do
-  if [ ! -z "${PLAY_STREAM_AUTH}" ]; then
+  if [ -n "${PLAY_STREAM_AUTH}" ]; then
     mpv --http-header-fields="Authorization:Basic ${PLAY_STREAM_AUTH}" --no-video --no-ytdl "${PLAY_STREAM_URL}"
   else
     echo "PLAY_STREAM_AUTH not set." 
     mpv --no-video --no-ytdl "${PLAY_STREAM_URL}"
   fi
+
+  if [ -n "${LOOP}" ] && [ "${LOOP}" = "no" ]; then
+    echo "LOOP=no. Exiting..."
+    exit 0
+  fi
+
   echo "sleeping for 10 seconds..."
   sleep 10
 done
