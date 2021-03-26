78c5cfea04177c51f942402d8998e1aee80cbd7c
Erik Ripley <erikripley@gmail.com>
2021-03-26 09:56:52 -0500

    Revert "allow specifying output file, log to stderr"
    This reverts commit a8b9d7a29c237b32901ee5b4d3de46c1a8cc783f.

--- test-my-speed.sh
+++ test-my-speed.sh
@@ -8,22 +8,16 @@
 #
 # Set the below OUTPUT_FILE variable to the path where you wish to store the results.
 
-log_error() {
-   >&2 echo "$1"
-}
-
-if [ -z "${OUTPUT_FILE}" ]; then
-   OUTPUT_FILE="${HOME}/Documents/notes/speedtest_results.txt"
-fi
+OUTPUT_FILE="${HOME}/Documents/notes/speedtest_results.txt"
 
 if [ ! -d "$(dirname "${OUTPUT_FILE}")" ]; then
-   log_error "The directory $(dirname "${OUTPUT_FILE}") does not exist. If you really want to output results there, please create it."
+   echo "The directory $(dirname "${OUTPUT_FILE}") does not exist. If you really want to output results there, please create it."
    exit 1
 fi
 
 
 if [ ! "$(command -v speedtest-cli)" ]; then
-   log_error "This script requires 'speedtest-cli' in the PATH."
+   echo "This script requires 'speedtest-cli' in the PATH."
    exit 1
 fi
 
