85a684799db117f1024c206572cf636de8b294e0
Erik Ripley <erikripley@gmail.com>
2022-01-21 17:06:35 -0600

    set light or dark theme wallpaper

--- set_desktop_wallpaper.sh
+++ set_desktop_wallpaper.sh
@@ -11,6 +11,16 @@ set_wallpaper_gnome() {
       exit 1
    fi
 
+   if [ -z "${DBUS_SESSION_BUS_ADDRESS}" ]; then
+
+      USERID="$(id -u)"
+      if ! GNOME_SESSION_PID="$(pgrep gnome-session -n -U "${USERID}")"; then
+         exit 0
+      fi
+
+      export "$(strings /proc/"${GNOME_SESSION_PID}"/environ | grep DBUS_SESSION_BUS_ADDRESS)"
+   fi
+
    gsettings set org.gnome.desktop.background picture-uri "file://${DESKTOP_PICTURE// /%20}"
    gsettings set org.gnome.desktop.screensaver picture-uri "file://${DESKTOP_PICTURE// /%20}"
 }
@@ -33,5 +43,25 @@ end tell
 EOF
 }
 
+WALLPAPER="${HOME}/Pictures/wallpapers/adwaita-day.png"
 
-set_wallpaper_gnome "${HOME}/Pictures/wallpapers/abstract.png"
+while getopts "dhp:" OPT; do
+   case "$OPT" in
+   "d")
+      WALLPAPER="${HOME}/Pictures/wallpapers/adwaita-night.png"
+   ;;
+   "p")
+      WALLPAPER="${OPTARG}"
+   ;;
+   "h"| *)
+      cat <<-EOS
+usage: $(basename "${BASH_SOURCE[0]}") [-d]
+      -d                   use dark theme
+      -p /path/to/image    specify path to a file to use
+EOS
+      exit 0;
+   ;;
+   esac
+done
+
+set_wallpaper_gnome "${WALLPAPER}"
