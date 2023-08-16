1153d9ba8a5033c8084a0c27d243347bd74049f8
Erik Ripley <erikripley@gmail.com>
2023-08-16 03:30:19 -0500

    add command line options

--- shuffle_pictures.rb
+++ shuffle_pictures.rb
@@ -2,8 +2,11 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'time'
 
-options = { program: :eog, slideshow_seconds: 3 }
+jpegview_sort = { file_name: 'n', modified_date: 'm', random: 'z' }
+
+options = { program: :eog, slideshow_seconds: 3, search_recursive: false, monitor: 0, sort: :file_name }
 OptionParser.new do |opts|
    opts.on("-e PATTERN", "--exclude PATTERN", Regexp, "specify a pattern to exclude") do |v|
       options[:exclude] = v
@@ -11,8 +14,18 @@ OptionParser.new do |opts|
    opts.on("-i PATTERN", "--include PATTERN", Regexp, "specify a pattern to include") do |v|
       options[:include] = v
    end
-   opts.on("-a", "--all", TrueClass, "start a slide show for each selection") do |_v|
-      options[:all] = true
+   # use -a no or -a false to set to false
+   opts.on("-a", "--all [FLAG]", TrueClass, "start a slide show for each selection") do |v|
+      options[:all] = v.nil? ? true : v
+   end
+   opts.on("--after DATE", String, "include directories with modified date after specified date") do |v|
+      options[:after] = Time.strptime(v, "%Y-%m-%d")
+   end
+   opts.on("--before DATE", String, "include directories with modified date before specified date") do |v|
+      options[:before] = Time.strptime(v, "%Y-%m-%d")
+   end
+   opts.on("--today", TrueClass, "include directories with modified date of today") do |_v|
+      options[:after] = Time.new(Date.today.year, Date.today.month, Date.today.day)
    end
    opts.on("-p PROGRAM", "--program PROGRAM", %i[jpegview eog], "the program to use for slide show") do |v|
       options[:program] = v
@@ -20,13 +33,23 @@ OptionParser.new do |opts|
    opts.on("-s SECONDS", Integer, "slide show seconds between each picture") do |v|
       options[:slideshow_seconds] = v
    end
+   opts.on("-r", TrueClass, "recurse into child directories") do |v|
+      options[:search_recursive] = v
+   end
+   opts.on("-m NUMBER", "--monitor NUMBER", Integer, "specify a monitor index 0,1,...") do |v|
+      options[:monitor] = v
+   end
 end.parse!
 
-selections = Dir.glob("**/*")
+puts options
+
+selections = (options[:search_recursive] ? Dir.glob("**/*") : Dir.entries('.').reject { |file| ['..', '.'].include?(file) })
    .select { |file| File.directory?(file) }
    .uniq
    .reject { |file| !options[:include].nil? and !options[:include].match(file) }
    .reject { |file| !options[:exclude].nil? and options[:exclude].match(file) }
+   .reject { |file| !options[:after].nil? and File.mtime(file) < options[:after] }
+   .reject { |file| !options[:before].nil? and File.mtime(file) > options[:before] }
    .select { |dir| Dir.entries(dir).any? { |file| ['.jpg', '.jpeg'].include?(File.extname(file).downcase) }}.shuffle
 
 selections = [selections.first] if selections.size > 1 and !options[:all]
@@ -45,7 +68,11 @@ until selections.empty?
       cmd << "\"#{selection}\""
       cmd << "/slideshow"
       cmd << options[:slideshow_seconds]
-      #cmd << "/fullscreen"
+      cmd << "/fullscreen"
+      cmd << "/monitor"
+      cmd << options[:monitor]
+      cmd << "/order"
+      cmd << jpegview_sort[options[:sort]]
       proc_pid = Process.spawn(cmd.join(" "))
       break if selections.size.eql?(0)
 
--- shuffle_videos.rb
+++ shuffle_videos.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'time'
 require 'tempfile'
-
 
 def shuffle(selections, count)
    count.nil? ? selections.shuffle : selections.sample(count)
@@ -20,8 +20,18 @@ OptionParser.new do |opts|
    opts.on("-c NUM", "--count NUM", Integer, "the number of selections to add to playlist") do |v|
       options[:count] = v
    end
-   opts.on("-a", "--all", TrueClass, "add all selections to the playlist") do |_v|
-      options[:count] = nil
+   # use -a no or -a false to set to false
+   opts.on("-a", "--all [FLAG]", TrueClass, "add all selections to the playlist") do |v|
+      options[:count] = v.eql?(false) ? 1 : nil
+   end
+   opts.on("--after DATE", String, "include files with modified date after specified date") do |v|
+      options[:after] = Time.strptime(v, "%Y-%m-%d")
+   end
+   opts.on("--before DATE", String, "include files with modified date before specified date") do |v|
+      options[:before] = Time.strptime(v, "%Y-%m-%d")
+   end
+   opts.on("--today", TrueClass, "include files with modified date of today") do |_v|
+      options[:after] = Time.new(Date.today.year, Date.today.month, Date.today.day)
    end
    opts.on("-m", "--mute", TrueClass, "mute video sound") do |v|
       options[:mute] = v
@@ -35,7 +45,9 @@ selections = shuffle(
    Dir.glob("**/*.{mp4,mov,m4v,mkv,avi}")
    .uniq
    .reject { |file| !options[:include].nil? and !options[:include].match(file) }
-   .reject { |file| !options[:exclude].nil? and options[:exclude].match(file) },
+   .reject { |file| !options[:exclude].nil? and options[:exclude].match(file) }
+   .reject { |file| !options[:after].nil? and File.mtime(file) < options[:after] }
+   .reject { |file| !options[:before].nil? and File.mtime(file) > options[:before] },
    options[:count]
 )
 exit(0) if selections.nil? or selections.size.eql?(0)
@@ -55,7 +67,7 @@ when :vlc
    cmd << "--loop"
    cmd << "--fullscreen"
    cmd << selections.map { |selection| "\"#{selection}\"" }
-   pid = Process.spawn(cmd.join(' '), out: "/dev/null", err: "/dev/null")
+   pid = Process.spawn(cmd.join(' '))
    puts pid
 when :mpv
    # mpv
