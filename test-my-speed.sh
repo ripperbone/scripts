38e21a834693c6d5466eb49c363b1c6fd5803ba4
Erik Ripley <erikripley@gmail.com>
2025-05-28 09:00:13 -0500

    install speedtest-cli in virtual env

--- test-my-speed.sh
+++ test-my-speed.sh
@@ -4,7 +4,9 @@
 # but I just want to dump all the output to a consistent place.
 #
 # I would suggest getting speedtest-cli the following way:
-# pip3 install --user speedtest-cli
+# python3 -m venv ~/.local/mypyenv
+# source ~/.local/mypyenv/bin/activate
+# pip3 install speedtest-cli
 #
 # Set the below OUTPUT_FILE variable to the path where you wish to store the results.
 
@@ -14,6 +16,9 @@ if [ ! -d "$(dirname "${OUTPUT_FILE}")" ]; then
    echo "The directory $(dirname "${OUTPUT_FILE}") does not exist. If you really want to output results there, please create it."
    exit 1
 fi
+
+
+source "${HOME}/.local/mypyenv/bin/activate"
 
 
 if [ ! "$(command -v speedtest-cli)" ]; then
